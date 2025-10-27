d30b32b6c3c138612d4c12fba58733aa0cf123da
Shaukat Ali <Shaukatalibans@gmail.com>
2025-10-27 12:30:10 +0500

    feat(backend): add basic authRoutes and update index.js

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+    res.send("signup route");
+});
+
+router.post("/signin", (req, res) => {
+    res.send("signin route");
+});
+
+router.post("/signout", (req, res) => {
+    res.send("signout route");
+});
+
+export default router;
