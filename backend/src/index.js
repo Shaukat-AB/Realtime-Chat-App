c05a767bc98d8a3bb7a4c68bc9e0eb079c045cd8
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-02 16:13:48 +0500

    update(backend): import cookieParser in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
 import { connectDb } from "./lib/db/db.js";
 
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
