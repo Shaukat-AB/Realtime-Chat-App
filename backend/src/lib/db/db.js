43db51e38e319be3ade5598bc82956fa210f098e
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-02 12:25:15 +0500

    feat(backend): add db.js and connect to mongoDB

---
+++ backend/src/lib/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+export const connectDb = async () => {
+    const MONGODB_URI = process.env.MONGODB_URI;
+
+    try {
+        const conn = await mongoose.connect(MONGODB_URI);
+        console.log("MongoDB connected: ", conn.connection.host);
+    } catch (err) {
+        console.log("MongoDB connection failed: ", err);
+    }
+};
