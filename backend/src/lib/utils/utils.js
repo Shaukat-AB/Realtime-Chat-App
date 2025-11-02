804948281138de0a1c8ef2414a8d825af7af79a6
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-02 14:59:32 +0500

    update(backend): export cookieName from utils.js

--- backend/src/lib/utils/utils.js
+++ backend/src/lib/utils/utils.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
 
+export const cookieName = "_auth";
+
 export const genToken = (userId, res) => {
     const JWT_SECRET = process.env.JWT_SECRET;
     const NODE_ENV = process.env.NODE_ENV;
 
     const expiresIn = "7d";
     const maxAge = 7 * 24 * 60 * 60 * 100; //mili seconds
-    const cookieName = "_auth";
 
     const token = jwt.sign({ userId }, JWT_SECRET, {
         expiresIn,
