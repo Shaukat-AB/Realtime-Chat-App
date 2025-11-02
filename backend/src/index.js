ca56b35ef457461caef9c6609bb9daebab280f20
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-02 12:26:43 +0500

    update(backend): import and call connectDB in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import { connectDb } from "./lib/db/db.js";
 
 dotenv.config();
 
@@ -11,4 +12,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running at port: " + PORT);
+    connectDb();
 });
