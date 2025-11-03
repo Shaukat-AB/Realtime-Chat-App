b6db8adba2e13132e30e04f06713654c3208c6cb
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-03 14:18:42 +0500

    feat(frontend): declear routes and create basic pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import { NavBar } from "./components";
+import {
+    HomePage,
+    ProfilePage,
+    SettingsPage,
+    SigninPage,
+    SignupPage,
+} from "./pages";
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <NavBar />
+            <Routes>
+                <Route index element={<HomePage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/signin" element={<SigninPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as HomePage } from "./HomePage";
+export { default as SignupPage } from "./SignupPage";
+export { default as SigninPage } from "./SigninPage";
+export { default as ProfilePage } from "./ProfilePage";
+export { default as SettingsPage } from "./SettingsPage";
