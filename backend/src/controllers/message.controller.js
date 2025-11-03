cb5fc49520fc750f2a6720cc22b3347ef3a56f4f
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-03 12:02:48 +0500

    feat(backend): create message.controller.js

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,70 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary/cloudinary.js";
+
+export const getUserContacts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const contacts = await User.find({ _id: { $ne: userId } }).select(
+            "-password"
+        );
+        res.status(200).json(contacts);
+    } catch (err) {
+        console.log("GetUserContacts message.controller error: ", err.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const getMessagesById = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {
+                    senderId: senderId,
+                    receiverId: receiverId,
+                },
+                {
+                    senderId: receiverId,
+                    receiverId: senderId,
+                },
+            ],
+        });
+
+        res.status(200).json(messages);
+    } catch (err) {
+        console.log("GetMessageById message.controller error: ", err.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            const uploaded = await cloudinary.uploader.upload(image);
+            imageUrl = uploaded.secure_url;
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        // Todo: add socket.io
+
+        res.status(201).json(newMessage);
+    } catch (err) {
+        console.log("SendMessage message.controller error: ", err.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
