2032ecf7e51aa6a7be2dc8ade939a956048338cd
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-09 13:10:28 +0500

    feat(frontend): create useVerifyAuth hook to verify auth user on web refresh

---
+++ frontend/src/hooks/queries/useVerifyAuth.js
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "../../store";
+
+export const useVerifyAuth = () => {
+    const { authUser, verifyAuth } = useAuthStore();
+
+    return useQuery({
+        queryKey: ["verifyAuth", authUser?._id],
+        queryFn: verifyAuth,
+    });
+};
