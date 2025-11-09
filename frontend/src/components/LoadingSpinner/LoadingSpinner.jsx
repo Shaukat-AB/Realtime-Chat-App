9417ef6b3570cae262c6cc15a2a259bb245085ca
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-09 13:12:57 +0500

    feat(frontend): create LoadingSpinner component

---
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+    return (
+        <div className="absolute w-full flex justify-center">
+            <span className="h-screen loading loading-spinner loading-lg"></span>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
