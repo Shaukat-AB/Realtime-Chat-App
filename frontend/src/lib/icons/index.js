d79f246cad520eb88b1fbcef36e23fb0f963794e
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-09 12:36:06 +0500

    feat(frontend): create lib/icons/index.js to export icons from heroicons

---
+++ frontend/src/lib/icons/index.js
@@ -0,0 +1,31 @@
+import {
+    ChatBubbleLeftIcon,
+    EyeIcon,
+    EyeSlashIcon,
+    UserIcon as UIcon,
+    UsersIcon as UsersI,
+    LockClosedIcon,
+    EnvelopeIcon,
+    Cog6ToothIcon,
+    ArrowRightStartOnRectangleIcon,
+    CameraIcon as CIcon,
+    ChevronDownIcon,
+    PaperAirplaneIcon,
+    PhotoIcon,
+    XMarkIcon,
+} from "@heroicons/react/24/outline";
+
+export const ChatSquareIcon = ChatBubbleLeftIcon;
+export const EyeOnIcon = EyeIcon;
+export const EyeOffIcon = EyeSlashIcon;
+export const UserIcon = UIcon;
+export const UsersIcon = UsersI;
+export const LockIcon = LockClosedIcon;
+export const MailIcon = EnvelopeIcon;
+export const SettingsIcon = Cog6ToothIcon;
+export const SignoutIcon = ArrowRightStartOnRectangleIcon;
+export const CameraIcon = CIcon;
+export const BtnArrowDownIcon = ChevronDownIcon;
+export const SendIcon = PaperAirplaneIcon;
+export const ImageIcon = PhotoIcon;
+export const XIcon = XMarkIcon;
