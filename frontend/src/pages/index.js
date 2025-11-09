a963ed820364027ae6dd21f6215c4feca414e145
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-09 13:19:15 +0500

    update(frontend): export pages from index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,4 +2,3 @@ export { default as HomePage } from "./HomePage";
 export { default as SignupPage } from "./SignupPage";
 export { default as SigninPage } from "./SigninPage";
 export { default as ProfilePage } from "./ProfilePage";
-export { default as SettingsPage } from "./SettingsPage";
