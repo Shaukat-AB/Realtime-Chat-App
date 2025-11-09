90e052b8f4e4ef8aec015496eb7af8beec774ec7
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-09 13:21:48 +0500

    update(frontend): update App.jsx and export components from index.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,47 @@
-import { Route, Routes } from "react-router-dom";
-import { NavBar } from "./components";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LoadingSpinner, NavBar } from "./components";
 import { HomePage, ProfilePage, SigninPage, SignupPage } from "./pages";
+import { useAuthStore } from "./store/useAuthStore";
 import { Toaster } from "react-hot-toast";
+import { useVerifyAuth } from "./hooks";
 
 const App = () => {
+    const { authUser } = useAuthStore();
+    const { isLoading } = useVerifyAuth();
+
+    if (isLoading && !authUser) {
+        return (
+            <>
+                <NavBar />
+                <LoadingSpinner />
+            </>
+        );
+    }
+
     return (
         <div>
             <NavBar />
             <Routes>
-                <Route index element={<HomePage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/signin" element={<SigninPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
+                <Route
+                    index
+                    element={
+                        authUser ? <HomePage /> : <Navigate to="/signin" />
+                    }
+                />
+                <Route
+                    path="/signup"
+                    element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/signin"
+                    element={!authUser ? <SigninPage /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        authUser ? <ProfilePage /> : <Navigate to="/signin" />
+                    }
+                />
             </Routes>
             <Toaster />
         </div>
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,3 @@
 export { default as NavBar } from "./NavBar/NavBar";
+export { default as Sidebar } from "./Sidebar/Sidebar";
+export { default as LoadingSpinner } from "./LoadingSpinner/LoadingSpinner";
