4b89b8446df693229bd406a0c7046873853e4f5a
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-25 11:38:06 +0500

    update(frontend): prevent smooth scroll on initial run in useRefScrollIntoView

--- frontend/src/hooks/useRefScrollIntoView.js
+++ frontend/src/hooks/useRefScrollIntoView.js
@@ -1,11 +1,14 @@
 import { useEffect, useRef } from 'react';
 
+let initial = true;
+
 export const useRefScrollIntoView = (deps = []) => {
   const ref = useRef(null);
 
   useEffect(() => {
     if (ref.current) {
-      ref.current.scrollIntoView({ behavior: 'smooth' });
+      ref.current.scrollIntoView(initial ? {} : { behavior: 'smooth' });
+      if (initial) initial = false;
     }
   }, [deps]);
 
