19cf893d8ddbb4699a2ca5331fc4a3669f8dfce0
Shaukat Ali <Shaukatalibans@gmail.com>
2025-12-04 15:55:00 +0500

    update(frontend): remove userEvent file since its unused and was added by mistake

--- backend/src/lib/socket/events/userEvent.js
+++
@@ -1,6 +0,0 @@
-const eventOnlineUser = (onlineUsers) => {
-  const userId = socket.handshake.query.userId;
-  if (userId) onlineUsers[userId] = socket.id;
-
-  io.emit(Events.EV_ONLINE_USERS, Object.keys(onlineUsers));
-};
