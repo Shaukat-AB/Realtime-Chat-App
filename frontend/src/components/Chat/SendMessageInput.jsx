e0839bb579919e6322a03b773a6a3a90de68db24
Shaukat Ali <Shaukatalibans@gmail.com>
2025-12-04 15:52:31 +0500

    update(frontend): update message input autofocus to true

--- frontend/src/components/Chat/SendMessageInput.jsx
+++ frontend/src/components/Chat/SendMessageInput.jsx
@@ -76,11 +76,12 @@ const SendMessageInput = ({ disabled = false }) => {
       <form onSubmit={handleSendMessage} className="flex items-center gap-2">
         <div className="flex-1 flex gap-2">
           <input
+            name="message"
             type="text"
             className="w-full input input-bordered rounded-lg input-sm sm:input-md"
             placeholder="Type a message..."
             value={text}
-            onChange={(e) => setText(e.target.value)}
+            autoFocus
           />
 
           <input
